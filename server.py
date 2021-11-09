a2b62755af09c5a591d957dbb7c361e05ef17ddf
tanzumt <89188092+tanzumt@users.noreply.github.com>
2021-11-09 19:19:01 +0900

    Update server.py

--- server.py
+++ server.py
@@ -8,4 +8,5 @@ def hello_world():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
